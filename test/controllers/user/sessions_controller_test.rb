398e44581886e5d1838144a4f8ee4d933c261dcf
Eric-Guo <eric.guocz@gmail.com>
2020-10-24 22:12:46 +0800

    Fix test.

--- test/controllers/user/sessions_controller_test.rb
+++ test/controllers/user/sessions_controller_test.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should sign in user-sign-in form" do
+  test 'should sign in user-sign-in form' do
     post user_session_path,
-      params: { user: { email: "eric@cloud-mes.com",
-                        password: "123456" } }
-    assert_response :redirect
+      params: { user: { email: "hackathon2020t2@wanxiang-blockchain.github",
+                        password: "hackathon2020t2" } }
+    assert_response :ok
   end
 end
