338cd7d2b7a5805da059742be30f712ab2de73fc
Eric-Guo <eric.guocz@gmail.com>
2020-10-24 22:22:02 +0800

    Should be redirect

--- test/controllers/user/sessions_controller_test.rb
+++ test/controllers/user/sessions_controller_test.rb
@@ -5,6 +5,6 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     post user_session_path,
       params: { user: { email: "hackathon2020t2@wanxiang-blockchain.github",
                         password: "hackathon2020t2" } }
-    assert_response :ok
+    assert_response :redirect # Success will go to home page.
   end
 end
