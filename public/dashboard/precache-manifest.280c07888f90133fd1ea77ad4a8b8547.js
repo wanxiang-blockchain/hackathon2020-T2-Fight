91740344fcad1f6afb1d0af53579a8d2975180ca
Eric-Guo <eric.guocz@gmail.com>
2020-10-25 13:28:20 +0800

    Release dashboard.

---
+++ public/dashboard/precache-manifest.280c07888f90133fd1ea77ad4a8b8547.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/1.47f00dfa6b22ac7158c7.css"
+  },
+  {
+    "url": "/bundle.main.47f00dfa6b22ac7158c7.js"
+  },
+  {
+    "revision": "a35e4ebc2dc3ab42cc6d18ee906b9b0d",
+    "url": "/index.html"
+  },
+  {
+    "url": "/main.47f00dfa6b22ac7158c7.css"
+  },
+  {
+    "url": "/vendors.47f00dfa6b22ac7158c7.js"
+  }
+]);
