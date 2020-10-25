e70fd0f7337c700d0be9c11cdd6ae503b48198c2
Eric-Guo <eric.guocz@gmail.com>
2020-10-25 08:02:29 +0800

    Display images.

--- app/controllers/veterans_controller.rb
+++ app/controllers/veterans_controller.rb
@@ -1,6 +1,6 @@
 class VeteransController < ApplicationController
   def index
     prepare_meta_tags title: t('.title')
-    @users = User.where.not(id: 1)
+    @users = User.where.not(user_picture_url: nil)
   end
 end
