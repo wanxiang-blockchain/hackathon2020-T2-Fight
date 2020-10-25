fca0f57c3cf11b3115031ebad9df4f9dd69f5134
Eric-Guo <eric.guocz@gmail.com>
2020-10-25 09:30:44 +0800

    Show search name.

--- app/controllers/veterans_controller.rb
+++ app/controllers/veterans_controller.rb
@@ -1,8 +1,9 @@
 class VeteransController < ApplicationController
   def index
     prepare_meta_tags title: t('.title')
+    @veteran_name = params[:veteran_name].presence
     @users = User.where.not(user_picture_url: nil).includes(:heart_rate_histories, :positions, :step_counts)
-    @users = @users.where("user_name LIKE ?", "%#{params[:veteran_name]}%") if params[:veteran_name].present?
+    @users = @users.where('user_name LIKE ?', "%#{params[:veteran_name]}%") if @veteran_name
     @users = @users.page(params[:page]).per(params[:per_page])
   end
 end
